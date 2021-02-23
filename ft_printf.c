49f88f7505d7c0a89232577494f7cf58979f480e
Camille LE LAN <cle-lan@e2r7p10.42.fr>
2021-02-23 16:31:18 +0100

    variadics macros working

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: cle-lan <cle-lan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/19 14:42:14 by cle-lan           #+#    #+#             */
-/*   Updated: 2021/02/23 15:49:40 by cle-lan          ###   ########.fr       */
+/*   Updated: 2021/02/23 16:30:29 by cle-lan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,29 +14,27 @@
 #include <stdio.h>
 #include "ft_printf.h"
 
-int     ft_printf(const char *input, ...)
+int     ft_printf(const char *format, ...)
 { 
     char res;
     char res1;
     char res2;
     va_list args;
 
-    va_start(args, input);
+    va_start(args, format);
     
-    // while (i < input)
-    // {
-    //     i = 0;
-    //     i++;
+
     res = va_arg(args, int);
     res1 = va_arg(args, int);
     res2 = va_arg(args, int);
-    //     printf("");
-    // }
-    ft_putstr((char *)input);
+
+    ft_putstr((char *)format);  //obligatoire
+    
+    //si ya des args les afficher avec putchar
     ft_putchar(res);
     ft_putchar(res1);
     ft_putchar(res2);
-    //printf("this is input %s\n", input);
+   
     va_end(args);
     return (0);
 }
