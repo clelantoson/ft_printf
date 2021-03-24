924c291539f6a17ade49ea9c670568b61c218021
Camille Le Lan-Toson <clelantoson@hotmail.fr>
2021-03-24 14:11:56 +0100

    44 test ok for s, modified ftputstrdot 304 in total

--- ft_putstrdot_count.c
+++ ft_putstrdot_count.c
@@ -6,7 +6,7 @@
 /*   By: cle-lan <cle-lan@42.student.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/23 17:20:09 by cle-lan           #+#    #+#             */
-/*   Updated: 2021/03/23 17:39:30 by cle-lan          ###   ########.fr       */
+/*   Updated: 2021/03/24 14:09:39 by cle-lan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ void	ft_putstrdot_count(char *s, t_flags *data, int precision)
 	i = 0;
 	if (s == NULL)
 		return ;
-	while (i < precision)
+	while (i < precision && s[i])
 	{
 		ft_putchar_count(s[i], data);
 		i++;
