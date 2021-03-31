4b7cc9838cb088cc2cdb15d684e0ff1a53b74061
Camille Le Lan-Toson <clelantoson@hotmail.fr>
2021-03-31 23:23:12 +0200

    96 test ok for s, 368 in total

--- ft_deal_with_str.c
+++ ft_deal_with_str.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_deal_with_str.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cle-lan <cle-lan@student.42.fr>            +#+  +:+       +#+        */
+/*   By: cle-lan <cle-lan@42.student.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/02 17:19:50 by cle-lan           #+#    #+#             */
-/*   Updated: 2021/03/31 17:17:34 by cle-lan          ###   ########.fr       */
+/*   Updated: 2021/03/31 23:18:32 by cle-lan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,7 +37,15 @@ void	ft_deal_with_str(char *str, t_flags *data)
 		}
 		else if (data->dot > 0)
 		{
-			ft_deal_with_dot(str, data);
+			if ((size_t)data->dot > ft_strlen(str))
+			{
+				data->dot = ft_strlen(str);
+				ft_deal_with_dot(str, data);
+			}
+			else
+			{
+				ft_deal_with_dot(str, data);
+			}
 		}
 	}
 	else if (data->minus == 1)
@@ -45,17 +53,19 @@ void	ft_deal_with_str(char *str, t_flags *data)
 		if (data->dot < 0)
 		{
 			ft_putstr_count(str, data);
-			ft_deal_with_width(data, ft_strlen(str));	
+			ft_deal_with_width(data, ft_strlen(str));
 		}
 		else if (data->dot > 0)
 		{
-			ft_putstr_count(str, data);
-			ft_deal_with_width(data, ft_strlen(str));
+
+				ft_putstr_count(str, data);
+			 	ft_deal_with_width(data, ft_strlen(str));
+			// }
 		}
 	}
 }
-	
-	// if (data->minus == 0 && data->dot < 0) //"%s" //"%5s" 
+
+	// if (data->minus == 0 && data->dot < 0) //"%s" //"%5s"
 	// {
 	// 	ft_deal_with_width(data, ft_strlen(str));
 	// 	ft_putstr_count(str, data);
