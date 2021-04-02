17682dd5c187439ed1f46dad192b459842bf7df1
Camille LE LAN <cle-lan@e2r6p12.42.fr>
2021-04-02 13:46:01 +0200

    refacto dealwstr, added width for d, 41 tests ok for d, 38 for i, 382 total

--- ft_deal_with_int.c
+++ ft_deal_with_int.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_deal_with_int.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cle-lan <cle-lan@42.student.fr>            +#+  +:+       +#+        */
+/*   By: cle-lan <cle-lan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/03 14:17:28 by cle-lan           #+#    #+#             */
-/*   Updated: 2021/03/03 19:33:32 by cle-lan          ###   ########.fr       */
+/*   Updated: 2021/04/02 13:42:09 by cle-lan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,13 @@
 
 void    ft_deal_with_int(int num, t_flags *data)
 {
+    char *i_to_s;
+    
+    i_to_s = ft_itoa(num);
+    
+    if (data->width > 0)
+    {
+        ft_deal_with_width(data, ft_strlen(i_to_s));
+    }
     ft_putnbr_count_chars(num, data);
 }
