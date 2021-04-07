a1e75166081f274582c1ad414977f00a031d31ff
Camille LE LAN <cle-lan@e2r7p10.42.fr>
2021-04-07 15:25:17 +0200

    51 test ok for d, 430 total

--- ft_deal_with_int.c
+++ ft_deal_with_int.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_deal_with_int.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cle-lan <cle-lan@42.student.fr>            +#+  +:+       +#+        */
+/*   By: cle-lan <cle-lan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/03 14:17:28 by cle-lan           #+#    #+#             */
-/*   Updated: 2021/04/07 12:28:22 by cle-lan          ###   ########.fr       */
+/*   Updated: 2021/04/07 15:22:44 by cle-lan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,11 +34,8 @@ void    ft_deal_with_int(int num, t_flags *data)
 		}
 		else if ((size_t)data->dot <= ft_strlen(i_to_s))
 		{
-			if (num > 0)
-			{
-				ft_deal_with_width(data, ft_strlen(i_to_s));
-				ft_putnbr_count_chars(num, data);
-			}
+			ft_deal_with_width(data, ft_strlen(i_to_s));
+			ft_putnbr_count_chars(num, data);
 		}
 	}
 
