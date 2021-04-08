7752d192bcb8cbc6599412122638619ea2d6d1a0
Camille Le Lan-Toson <clelantoson@hotmail.fr>
2021-04-08 11:46:14 +0200

    %8.5d test passed but broke other tests, still 51 test ok for d

--- ft_deal_with_int.c
+++ ft_deal_with_int.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_deal_with_int.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cle-lan <cle-lan@student.42.fr>            +#+  +:+       +#+        */
+/*   By: cle-lan <cle-lan@42.student.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/03 14:17:28 by cle-lan           #+#    #+#             */
-/*   Updated: 2021/04/07 18:23:06 by cle-lan          ###   ########.fr       */
+/*   Updated: 2021/04/08 11:24:02 by cle-lan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,10 +35,10 @@ void    ft_deal_with_int(int num, t_flags *data)
 		else if ((size_t)data->dot <= ft_strlen(i_to_s))
 		{
 			ft_deal_with_width(data, ft_strlen(i_to_s));
-			
 			ft_putnbr_count_chars(num, data);
 		}
 	}
+
 
 	// if (data->width > 0 && data->dot > 0)
 	// {
@@ -51,19 +51,35 @@ void    ft_deal_with_int(int num, t_flags *data)
 	{
 		if (data->minus == 0)
 		{
-			ft_deal_with_width(data, ft_strlen(i_to_s));
-			ft_putnbr_count_chars(num, data);
-			free(i_to_s);
-			if (data->dot > 0)
+
+			// if (data->dot > 0)
+			// {
+			// 	if (data->width > 0)
+			// 	{
+			// 		ft_deal_with_width(data, ft_strlen(i_to_s));
+			// 		free(i_to_s);
+			// 		ft_putnbr_count_chars(num, data);
+			// 	}
+			// 	// if (copy_num < 0 && data->dot >= 0)
+			// 	// 	ft_putstr_count("-", data);
+			// }
+			if (data->dot >= 0)
 			{
-				if (data->width > 0)
-				{
-					ft_deal_with_width(data, ft_strlen(i_to_s));
-					free(i_to_s);
-					ft_putnbr_count_chars(num, data);
-				}
-				// if (copy_num < 0 && data->dot >= 0)
-				// 	ft_putstr_count("-", data);
+				data->width -= data->dot;
+				ft_deal_with_width(data, 0);
+			}
+			if (data->dot >= 0)
+			{
+				data->width = data->dot - 1;
+				data->zero = 1;
+				ft_deal_with_width(data, ft_strlen(i_to_s) -1);
+				ft_putstrdot_count(i_to_s, data, ft_strlen(i_to_s));
+			}
+			else
+			{
+				ft_deal_with_width(data, ft_strlen(i_to_s));
+				ft_putnbr_count_chars(num, data);
+				free(i_to_s);
 			}
 		}
 		else if (data->minus == 1)
