0e8f5cf5e64bb5c653f0f9e5849af0953399be4c
Camille LE LAN <cle-lan@e2r6p10.42.fr>
2021-04-13 15:37:14 +0200

    fixed 04.d -135 for printflover

--- ft_deal_with_int.c
+++ ft_deal_with_int.c
@@ -6,7 +6,7 @@
 /*   By: cle-lan <cle-lan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/03 14:17:28 by cle-lan           #+#    #+#             */
-/*   Updated: 2021/04/13 15:15:16 by cle-lan          ###   ########.fr       */
+/*   Updated: 2021/04/13 15:35:57 by cle-lan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,7 +33,7 @@ void			ft_deal_with_int(int num, t_flags *data)
 	}
 	if (num < 0 && (data->dot > 0 || data->zero == 1))
 	{
-		if (data->zero == 1 && (data->dot == -1 || data->width == 0))
+		if (data->zero == 1 && (data->dot <= 0 || data->width == 0))
 			ft_putstr_count("-", data);
 		num *= -1;
 		data->zero = 1;
