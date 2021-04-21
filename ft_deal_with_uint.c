3936f1d5575733a9c9750d189220155f35bc8c13
Camille Le Lan-Toson <clelantoson@hotmail.fr>
2021-04-22 01:00:12 +0200

    utin normed

--- ft_deal_with_uint.c
+++ ft_deal_with_uint.c
@@ -12,17 +12,37 @@
 
 #include "ft_printf.h"
 
+void	ft_print_width_and_dot_uint(t_flags *data, int copy_dot, int copy_width, char *numstr)
+{
+	if (data->dot > 0)
+	{
+		data->width = data->dot - 1;
+		data->zero = 1;
+		ft_deal_with_width(data, ft_strlen(numstr) - 1);
+	}
+	ft_putstrdot_count(numstr, data, ft_strlen(numstr));
+	data->dot = copy_dot;
+	data->zero = 0;
+	data->width = copy_width;
+}
+
+void		ft_minus_or_no_minus_uint(t_flags *data, int copy_dot, int copy_width, char *numstr)
+{
+	if (data->minus == 1)
+		ft_print_width_and_dot_uint(data, copy_dot, copy_width, numstr);
+	ft_print_width_int(data, numstr);
+	if (data->minus == 0)
+		ft_print_width_and_dot_uint(data, copy_dot, copy_width, numstr);
+}
+
 void				ft_deal_with_uint(unsigned int num, t_flags *data)
 {
-	char			*numstr;
-	int				copy_num;
-	int 			copy_dot;
-	int 			copy_width;
+	char	*numstr;
+	int		copy_dot;
+	int		copy_width;
 
 	copy_dot = data->dot;
 	copy_width = data->width;
-	copy_num = num;
-
 	if (data->zero == 1 && data->dot >= 0)
 		data->zero = 0;
 	if (data->dot == 0 && num == 0)
@@ -31,48 +51,6 @@ void				ft_deal_with_uint(unsigned int num, t_flags *data)
 		return ;
 	}
 	numstr = ft_utoa(num);
-	if (data->minus == 1)
-	{
-		if (data->dot > 0)
-		{
-			data->width = data->dot - 1;
-			data->zero = 1;
-			ft_deal_with_width(data, ft_strlen(numstr) - 1);
-		}
-		ft_putstrdot_count(numstr, data, ft_strlen(numstr));
-		data->dot = copy_dot;
-		data->zero = 0;
-		data->width = copy_width;
-	}
-	if (data->dot > 0 && (size_t)data->dot < ft_strlen(numstr))
-		data->dot = ft_strlen(numstr);
-
-	if (data->dot > 0)
-	{
-		data->width -= data->dot;
-		data->zero = 0;
-		ft_deal_with_width(data, 0);
-	}
-	else
-		ft_deal_with_width(data, ft_strlen(numstr));
-
-	if (data->minus == 0)
-	{
-		if (data->dot >= 0)
-		{
-			data->width = data->dot - 1;
-			data->zero = 1;
-			ft_deal_with_width(data, ft_strlen(numstr) - 1);
-		}
-		ft_putstrdot_count(numstr, data, ft_strlen(numstr));
-		data->dot = copy_dot;
-		data->zero = 0;
-		data->width = copy_width;
-	}
+	ft_minus_or_no_minus_uint(data, copy_dot, copy_width, numstr);
 	free(numstr);
 }
-
-
-//  Si le nombre de chiffres dans l’argument est inférieur à precision , la valeur de sortie est
-//   remplie à gauche de zéros.
-//   La valeur n’est pas tronquée lorsque le nombre de chiffres dépasse la précision
