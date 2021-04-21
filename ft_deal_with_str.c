74771048dfc9c0249e4bc0fbf831185bf873a911
Camille LE LAN <cle-lan@e2r7p10.42.fr>
2021-04-21 15:23:05 +0200

    norm ok for dealwithstr

--- ft_deal_with_str.c
+++ ft_deal_with_str.c
@@ -6,41 +6,32 @@
 /*   By: cle-lan <cle-lan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/02 17:19:50 by cle-lan           #+#    #+#             */
-/*   Updated: 2021/04/21 12:55:27 by cle-lan          ###   ########.fr       */
+/*   Updated: 2021/04/21 15:20:33 by cle-lan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-void	ft_print_no_minus(char *str, t_flags *data)
+void	ft_print(char *str, t_flags *data, int temp)
 {
-	ft_deal_with_width(data, data->dot);
-	ft_putstrdot_count(str, data, data->dot);
-}
-
-void	ft_print_with_minus(char *str, t_flags *data)
-{
-	ft_putstrdot_count(str, data, data->dot);
-	ft_deal_with_width(data, temp);
-}
-
-void		ft_zero_or_nulldot(char *str, t_flags *data)
-{
-	if (!str)
-		str = "(null)";
-	if (ft_strlen(str) == 0)
-		ft_deal_with_width(data, ft_strlen(str));
-	if (data->dot == 0)
+	if (data->minus == 0)
 	{
-		if (data->width > 0)
-			ft_deal_with_width(data, 0);
-		else
-			ft_putstrdot_count(str, data, data->dot);
+		ft_deal_with_width(data, data->dot);
+		ft_putstrdot_count(str, data, data->dot);
+	}
+	if (data->minus == 1)
+	{
+		temp = data->dot;
+		ft_putstrdot_count(str, data, data->dot);
+		ft_deal_with_width(data, temp);
+		data->dot = 0;
 	}
 }
 
-void	ft_no_minus_str(char *str, t_flags *data)
+void	ft_no_minus_str(char *str, t_flags *data, int temp)
 {
+	temp = 0;
+
 	if (data->dot < 0)
 	{
 		ft_deal_with_width(data, ft_strlen(str));
@@ -51,14 +42,14 @@ void	ft_no_minus_str(char *str, t_flags *data)
 		if ((size_t)data->dot > ft_strlen(str))
 		{
 			data->dot = ft_strlen(str);
-			ft_print_no_minus(str, data);
+			ft_print(str, data, temp);
 		}
 		else
-			ft_print_no_minus(str, data);
+			ft_print(str, data, temp);
 	}
 }
 
-void 	ft_minus_str(char *str, t_flags *data, int temp)
+void	ft_minus_str(char *str, t_flags *data, int temp)
 {
 	if (data->dot == 1)
 	{
@@ -67,12 +58,7 @@ void 	ft_minus_str(char *str, t_flags *data, int temp)
 			if (ft_strlen(str) == 0)
 				ft_deal_with_width(data, ft_strlen(str));
 			else
-			{
-				temp = data->dot;
-				ft_putstrdot_count(str, data, data->dot);
-				ft_deal_with_width(data, temp);
-				data->dot = 0;
-			}
+				ft_print(str, data, temp);
 		}
 		else
 			ft_deal_with_width(data, 0);
@@ -87,11 +73,7 @@ void 	ft_minus_str(char *str, t_flags *data, int temp)
 		if ((size_t)data->dot > ft_strlen(str))
 			data->dot = ft_strlen(str);
 		if (data->dot > 0)
-		{
-			temp = data->dot;
-			ft_putstrdot_count(str, data, data->dot);
-			ft_deal_with_width(data, temp);
-		}
+			ft_print(str, data, temp);
 	}
 }
 
@@ -99,10 +81,20 @@ void	ft_deal_with_str(char *str, t_flags *data)
 {
 	int temp;
 
-	if (!str || ft_strlen(str) == 0 || data->dot == 0)
-		ft_zero_or_nulldot(str, data);
+	temp = 0;
+	if (!str)
+		str = "(null)";
+	if (ft_strlen(str) == 0)
+		ft_deal_with_width(data, ft_strlen(str));
+	if (data->dot == 0)
+	{
+		if (data->width > 0)
+			ft_deal_with_width(data, 0);
+		else
+			ft_putstrdot_count(str, data, data->dot);
+	}
 	else if (data->minus == 0)
-		ft_no_minus_str(str, data);
+		ft_no_minus_str(str, data, temp);
 	else if (data->minus == 1)
 	{
 		ft_minus_str(str, data, temp);
