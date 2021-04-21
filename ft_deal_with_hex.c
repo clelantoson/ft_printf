b2c42c945644bedda6161e4f424106d700c1d088
Camille LE LAN <cle-lan@e2r7p10.42.fr>
2021-04-21 17:18:21 +0200

    refacto for norm ok for hexa

--- ft_deal_with_hex.c
+++ ft_deal_with_hex.c
@@ -6,68 +6,24 @@
 /*   By: cle-lan <cle-lan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/05 20:58:39 by cle-lan           #+#    #+#             */
-/*   Updated: 2021/04/21 12:20:00 by cle-lan          ###   ########.fr       */
+/*   Updated: 2021/04/21 17:16:49 by cle-lan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-void			ft_deal_with_hex(unsigned int to_hexa, int to_lower, t_flags *data)
+void		ft_is_hexa_neg(t_flags *data, int copy_width, unsigned int to_hexa)
 {
-	char	*hexa;
-	int		copy_hex;
-	int		copy_dot;
-	int		copy_width;
-	int		neg;
+	if (to_hexa < 0)
+		data->width = copy_width - 1;
+	else
+		data->width = copy_width;
+}
 
-	neg = 0;
-	copy_dot = data->dot;
-	copy_width = data->width;
-	copy_hex = to_hexa;
-
-	if (data->zero == 1 && data->dot >= 0)
-		data->zero = 0;
-	if (data->dot == 0 && to_hexa == 0)
-	{
-		ft_deal_with_width(data, 0);
-		return ;
-	}
-	if (to_hexa < 0 && (data->dot > 0 || data->zero == 1))
-	{
-		if (data->zero == 1 && (data->dot <= 0 || data->width == 0))
-			ft_putstr_count("-", data);
-		to_hexa  = -to_hexa;
-		data->zero = 1;
-		data->width--;
-		data->zero = 1;
-		neg = 1;
-	}
-	
-	hexa = ft_ulltoa_base((unsigned long long)to_hexa, 16);
-		
-	if (to_lower == 1)
-		hexa = ft_str_tolower(hexa);
-	if (data->minus == 1)	
-	{
-		// if (copy_hex < 0 && data->dot > 0)
-		// 	ft_putchar_count('-', data);
-		if (data->dot > 0)
-		{
-			data->width = data->dot - 1;
-			data->zero = 1;
-			ft_deal_with_width(data, ft_strlen(hexa) - 1);
-		}
-		ft_putstrdot_count(hexa, data, ft_strlen(hexa));
-		data->dot = copy_dot;
-		data->zero = 0;
-		if (neg == 1)
-			data->width = copy_width -1;
-		else
-			data->width = copy_width;
-	}
+void		ft_print_width(t_flags *data, char *hexa)
+{
 	if (data->dot > 0 && (size_t)data->dot < ft_strlen(hexa))
 		data->dot = ft_strlen(hexa);
-
 	if (data->dot > 0)
 	{
 		data->width -= data->dot;
@@ -76,25 +32,57 @@ void			ft_deal_with_hex(unsigned int to_hexa, int to_lower, t_flags *data)
 	}
 	else
 		ft_deal_with_width(data, ft_strlen(hexa));
+}
 
+void		ft_print_width_and_dot(t_flags *data, int copy_dot, char *hexa)
+{
+	if (data->dot > 0)
+	{
+		data->width = data->dot - 1;
+		data->zero = 1;
+		ft_deal_with_width(data, ft_strlen(hexa) - 1);
+	}
+	ft_putstrdot_count(hexa, data, ft_strlen(hexa));
+	data->dot = copy_dot;
+	data->zero = 0;
+}
+
+int			ft_check_nuldot_argnul(t_flags *data, unsigned int to_hexa)
+{
+	if (data->dot == 0 && to_hexa == 0)
+	{
+		ft_deal_with_width(data, 0);
+		return (1);
+	}
+	else
+		return (0);
+}
+
+void		ft_deal_with_hex(unsigned int to_hexa, int to_lower, t_flags *data)
+{
+	char	*hexa;
+	int		copy_dot;
+	int		copy_width;
+
+	copy_dot = data->dot;
+	copy_width = data->width;
+	if (data->zero == 1 && data->dot >= 0)
+		data->zero = 0;
+	if (ft_check_nuldot_argnul(data, to_hexa) == 1)
+		return ;
+	hexa = ft_ulltoa_base((unsigned long long)to_hexa, 16);
+	if (to_lower == 1)
+		hexa = ft_str_tolower(hexa);
+	if (data->minus == 1)
+	{
+		ft_print_width_and_dot(data, copy_dot, hexa);
+		ft_is_hexa_neg(data, copy_width, to_hexa);
+	}
+	ft_print_width(data, hexa);
 	if (data->minus == 0)
 	{
-		// if (copy_hex < 0 && data->dot > 0)
-		// 	ft_putchar_count('-', data);
-		if (data->dot > 0)
-		{
-			data->width = data->dot - 1;
-			data->zero = 1;
-			ft_deal_with_width(data, ft_strlen(hexa) - 1);
-		}
-		ft_putstrdot_count(hexa, data, ft_strlen(hexa));
-		data->dot = copy_dot;
-		data->zero = 0;
-		if (neg == 1)
-			data->width = copy_width -1;
-		else
-			data->width = copy_width;
+		ft_print_width_and_dot(data, copy_dot, hexa);
+		ft_is_hexa_neg(data, copy_width, to_hexa);
 	}
 	free(hexa);
 }
-
